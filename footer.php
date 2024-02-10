139c50f793837d94ac77ed53c07f6ce27b46132f
joshua0200 <jmalcause00@gmail.com>
2024-02-10 13:01:13 +0800

    v1.0

---
+++ footer.php
@@ -0,0 +1,7 @@
+<footer id="footer">
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong><span>Joshua Marc Alcause</span></strong>
+      </div>
+    </div>
+  </footer>
